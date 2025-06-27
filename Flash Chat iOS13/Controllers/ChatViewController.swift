d720e70e03c00c272805e9f1d336ad20de66b478
Gururaja M <gururajkumar161@gmail.com>
2025-06-28 04:38:25 +0530

    Added the date key to fireBaseStore

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -34,7 +34,7 @@ class ChatViewController: UIViewController {
     
     func loadMessages() {
       
-        db.collection(K.FStore.collectionName).addSnapshotListener { (QuerySnapshot,error) in
+        db.collection(K.FStore.collectionName).order(by:K.FStore.dateField).addSnapshotListener { (QuerySnapshot,error) in
             self.messages = []
             if let error = error {
                 print("Error getting documents: \(error)")
@@ -59,7 +59,7 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: UIButton) {
         if let messageBody = messageTextfield.text , let messageSender = Auth.auth().currentUser!.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { error in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody,K.FStore.dateField : Date().timeIntervalSince1970]) { error in
                 if let e = error {
                     print("Error adding document: \(e)")
                 } else {
