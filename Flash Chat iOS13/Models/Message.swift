a6a65dbf156fc2405053f4d59ac44df98d34dbcf
Gururaja M <gururajkumar161@gmail.com>
2025-06-28 01:49:31 +0530

    Created the message model

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Gururaja M on 28/06/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let body: String
+    let sender: String
+    
+}
+
+
